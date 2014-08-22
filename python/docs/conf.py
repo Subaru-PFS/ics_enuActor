c0c6192b1a4e9a6b48468f3492f488db2b2973a8
Thomas PEGOT-OGIER <thomas.pegot@lam.fr>
2014-08-22 11:03:25 +0200

    Updated/Compilation doc: - update class diagram image related to new architecture from   FactoryDevlpt branch - little update state machine

--- python/docs/conf.py
+++ python/docs/conf.py
@@ -125,15 +125,16 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'bootstrap' #cloud
+html_theme = 'cloud' #bootstrap
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {'bootswatch_theme': "cosmo"}#{"rightsidebar": True,}
+html_theme_options = {"roottarget": "enuActor"}#{'bootswatch_theme': "cosmo"}
 
 # Add any path that contain custom themes here, relative to this directory.
-html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()#[csp.get_theme_dir()]
+#html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()#[csp.get_theme_dir()]
+html_theme_path = [csp.get_theme_dir()]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
