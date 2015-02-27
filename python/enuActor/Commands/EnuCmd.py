dbae210b3a6890f8e642277337d22157847e2229
Thomas PEGOT-OGIER <thomas.pegot@lam.fr>
2015-02-27 18:39:10 +0100

    Enu help done

--- python/enuActor/Commands/EnuCmd.py
+++ python/enuActor/Commands/EnuCmd.py
@@ -4,7 +4,7 @@ import sys
 import opscore.protocols.keys as keys
 import opscore.protocols.types as types
 from opscore.utility.qstr import qstr
-
+from enuActor.MyFSM import FysomError
 
 class EnuCmd(object):
 
@@ -12,11 +12,16 @@ class EnuCmd(object):
         self.actor = actor
         self.vocab = [
             ('status', '', self.status),
-            ('start', '[@(operation|simulation)]', self.start),
+            ('start', '', self.start),
             ('@(off|load|busy|idle|SafeStop|fail)', '', self.set_state),
             ('init', '', self.init),
             ('ping', '', self.ping),
              ]
+
+        # Define typed command arguments for the above commands.
+        self.keys = keys.KeysDictionary("enu_enu", (1, 1),
+                                        )
+
 
     def ping(self, cmd):
         """Query the actor for liveness/happiness."""
@@ -24,14 +29,21 @@ class EnuCmd(object):
         cmd.finish("text='Enu Actor: o/'")
 
     def init(self, cmd):
+        """ Initialise Enu device
+
+        """
         self.actor.enu.initialise()
 
     def start(self, cmd):
+        """ Start and initialise Enu actor"""
         self.actor.enu.startUp()
         self.actor.enu.initialise()
         self.actor.enu.finish("start operation done successfully!")
 
     def set_state(self, cmd):
+        """ Change current Enu state to BUSY, IDLE, LOADED, ...
+
+        """
         state = cmd.cmd.keywords[0].name
         try:
             getattr(self.actor.enu.fsm, state)()
