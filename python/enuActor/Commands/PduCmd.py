eb77efc5e68765afe1d12b7423b2d2c7c8aa4cc4
Arnaud Le Fur <arnaud.lefur@lam.fr>
2019-01-23 12:26:12 +0100

    mimic dcb pdu behaviour

--- python/enuActor/Commands/PduCmd.py
+++ python/enuActor/Commands/PduCmd.py
@@ -2,12 +2,13 @@
 
 
 import opscore.protocols.keys as keys
+import opscore.protocols.types as types
 from enuActor.utils.wrap import threaded
+
 
 class PduCmd(object):
     def __init__(self, actor):
         # This lets us access the rest of the actor.
-        self.name = "pdu"
         self.actor = actor
 
         # Declare the commands we implement. When the actor is started
@@ -15,13 +16,19 @@ class PduCmd(object):
         # associated methods when matched. The callbacks will be
         # passed a single argument, the parsed and typed command.
         #
+        self.name = "pdu"
         self.vocab = [
-            ('pdu', 'status', self.status),
+            (self.name, 'status', self.status),
+            ('power', '[<on>] [<off>]', self.switch),
 
         ]
 
         # Define typed command arguments for the above commands.
-        self.keys = keys.KeysDictionary("enu_pdu", (1, 1),
+        self.keys = keys.KeysDictionary("enu__pdu", (1, 1),
+                                        keys.Key("on", types.String() * (1, None),
+                                                 help='which channel to switch on.'),
+                                        keys.Key("off", types.String() * (1, None),
+                                                 help='which channel to switch off.'),
                                         )
 
     @property
@@ -31,8 +38,29 @@ class PduCmd(object):
         except KeyError:
             raise RuntimeError('%s controller is not connected.' % (self.name))
 
+    @property
+    def slit(self):
+        try:
+            return self.actor.controllers['slit']
+        except KeyError:
+            raise RuntimeError('slit controller is not connected.')
+
     @threaded
     def status(self, cmd):
-        """Report state, mode, position"""
+        """Report status and version; obtain and send current data"""
 
+        self.controller.getStatus(cmd)
+
+    @threaded
+    def switch(self, cmd):
+        cmdKeys = cmd.cmd.keywords
+
+        switchOn = cmdKeys['on'].values if 'on' in cmdKeys else []
+        switchOff = cmdKeys['off'].values if 'off' in cmdKeys else []
+
+        for channel in switchOn + switchOff:
+            if channel not in self.controller.powerPorts:
+                raise ValueError('unknown port')
+
+        self.controller.substates.switch(cmd=cmd, switchOn=switchOn, switchOff=switchOff)
         self.controller.getStatus(cmd)
--- python/enuActor/Controllers/pdu.py
+++ python/enuActor/Controllers/pdu.py
@@ -1,5 +1,7 @@
 __author__ = 'alefur'
 import logging
+import time
+
 import enuActor.Controllers.bufferedSocket as bufferedSocket
 from actorcore.FSM import FSMDev
 from actorcore.QThread import QThread
@@ -7,6 +9,7 @@ from enuActor.simulator.pdu_simu import PduSim
 
 
 class pdu(FSMDev, QThread, bufferedSocket.EthComm):
+    powerPorts = ('slit', 'xenon', 'hgar', 'krypton')
 
     def __init__(self, actor, name, loglevel=logging.DEBUG):
         """__init__.
@@ -16,19 +19,28 @@ class pdu(FSMDev, QThread, bufferedSocket.EthComm):
         :param name: controller name
         :type name: str
         """
+        substates = ['IDLE', 'SWITCHING', 'FAILED']
+        events = [{'name': 'switch', 'src': 'IDLE', 'dst': 'SWITCHING'},
+                  {'name': 'idle', 'src': ['SWITCHING', ], 'dst': 'IDLE'},
+                  {'name': 'fail', 'src': ['SWITCHING', ], 'dst': 'FAILED'},
+                  ]
 
         bufferedSocket.EthComm.__init__(self)
         QThread.__init__(self, actor, name)
-        FSMDev.__init__(self, actor, name)
+        FSMDev.__init__(self, actor, name, events=events, substates=substates)
 
-        self.sock = None
-        self.sim = None
-        self.EOL = '\r\n'
-
-        self.ioBuffer = bufferedSocket.BufferedSocket(self.name + "IO", EOL='\r\n')
+        self.addStateCB('SWITCHING', self.switch)
 
         self.logger = logging.getLogger(self.name)
         self.logger.setLevel(loglevel)
+
+        self.ioBuffer = bufferedSocket.BufferedSocket(self.name + 'IO', EOL='\r\n>')
+        self.EOL = '\r\n'
+
+        self.mode = ''
+        self.sim = None
+
+        self.state = {}
 
     @property
     def simulated(self):
@@ -39,6 +51,9 @@ class pdu(FSMDev, QThread, bufferedSocket.EthComm):
         else:
             raise ValueError('unknown mode')
 
+    def getOutlet(self, channel):
+        return self.actor.config.get('outlets', channel).strip().zfill(2)
+
     def start(self, cmd=None, doInit=True, mode=None):
         QThread.start(self)
         FSMDev.start(self, cmd=cmd, doInit=doInit, mode=mode)
@@ -48,17 +63,19 @@ class pdu(FSMDev, QThread, bufferedSocket.EthComm):
         self.exit()
 
     def loadCfg(self, cmd, mode=None):
-        """loadCfg
-        load Configuration file
-        :param cmd
-        :param mode (operation or simulation, loaded from config file if None
-        :return: True, ret : Config File successfully loaded'
-                 False, ret : Config file badly formatted, Exception ret
-        """
+        """| Load Configuration file. called by device.loadDevice()
 
-        self.mode = self.actor.config.get('pdu', 'mode') if mode is None else mode
+        :param cmd: on going command
+        :param mode: operation|simulation, loaded from config file if None
+        :type mode: str
+        :raise: Exception Config file badly formatted
+        """
         self.host = self.actor.config.get('pdu', 'host')
         self.port = int(self.actor.config.get('pdu', 'port'))
+        self.mode = self.actor.config.get('pdu', 'mode') if mode is None else mode
+
+        for channel in self.powerPorts:
+            self.getOutlet(channel=channel)
 
     def startComm(self, cmd):
         """| Start socket with the interlock board or simulate it.
@@ -67,53 +84,90 @@ class pdu(FSMDev, QThread, bufferedSocket.EthComm):
         :param cmd: on going command,
         :raise: Exception if the communication has failed with the controller
         """
-
-        self.sim = PduSim()  # Create new simulator
-        s = self.connectSock()
-
-    def init(self, cmd):
-        """ Initialise the temperature controller
-
-        wrapper @safeCheck handles the state machine
-        :param e : fsm event
-        :return: True, ret : if every steps are successfully operated, fsm (LOADED => IDLE)
-                 False, ret : if a command fail, user if warned with error ret, fsm (LOADED => FAILED)
-        """
-
-        ret = self.sendOneCommand("init", doClose=False, cmd=cmd)
-
-    def getStatus(self, cmd):
-        """getStatus
-        temperature is nan if the controller is unreachable
-        :param cmd,
-        :return True, state, mode, 8*temperature
-                 False, state, mode, 8*nan if not initialised or an error had occured
-        """
-
-        cmd.inform('pduFSM=%s,%s' % (self.states.current, self.substates.current))
         cmd.inform('pduMode=%s' % self.mode)
 
-        if self.states.current == 'ONLINE':
-            self.checkStatus(cmd)
+        self.sim = PduSim()
+        s = self.connectSock()
 
-        cmd.finish()
-
-    def checkStatus(self, cmd):
-        """fetchTemps
-        temperature is nan if the controller is unreachable
-        :param cmd,
-        :return True, 8*temperature
-                 False, 8*nan if not initialised or an error had occured
-        """
+    def switch(self, e):
+        toSwitch = dict([(channel, 'on') for channel in e.switchOn] + [(channel, 'off') for channel in e.switchOff])
 
         try:
-            ret = self.sendOneCommand("status", cmd=cmd)
+            for channel, state in toSwitch.items():
+                cmdStr = "sw o%s %s imme" % (self.getOutlet(channel=channel), state)
+                ret = self.sendOneCommand(cmdStr=cmdStr, cmd=e.cmd, doRaise=False)
+                self.checkChannel(cmd=e.cmd, channel=channel)
+                time.sleep(2)
+
+            self.substates.idle(cmd=e.cmd)
 
         except:
-            cmd.warn('pdu=unknown')
+            self.substates.fail(cmd=e.cmd)
             raise
 
-        cmd.inform('pdu=%s' % ret)
+        finally:
+            self.closeSock()
+
+    def checkChannel(self, cmd, channel):
+
+        outlet = self.getOutlet(channel=channel)
+
+        ret = self.sendOneCommand('read status o%s format' % outlet, cmd=cmd)
+        __, outlet, state = ret.rsplit(' ', 2)
+
+        self.setState(cmd=cmd, outlet=outlet, channel=channel, state=state)
+
+    def getStatus(self, cmd):
+        cmd.inform('pduMode=%s' % self.mode)
+        cmd.inform('pduFSM=%s,%s' % (self.states.current, self.substates.current))
+
+        channels = [channel for channel in self.actor.config.options('outlets')]
+
+        if self.states.current == 'ONLINE':
+            try:
+                for channel in channels:
+                    self.checkChannel(cmd=cmd, channel=channel)
+
+                cmd.finish('pduVAW=%s,%s,%s' % self.checkVaw(cmd))
+
+            except:
+                raise
+
+            finally:
+                self.closeSock()
+
+    def checkVaw(self, cmd):
+
+        voltage = self.sendOneCommand('read meter dev volt simple', cmd=cmd)
+        current = self.sendOneCommand('read meter dev curr simple', cmd=cmd)
+        power = self.sendOneCommand('read meter dev pow simple', cmd=cmd)
+
+        return voltage, current, power
+
+    def setState(self, cmd, outlet, channel, state):
+
+        self.state[channel] = state
+        cmd.inform('pduport%s=%s,%s' % (outlet, channel, state))
+
+    def connectSock(self):
+        """ Connect socket if self.sock is None
+
+        :param cmd : current command,
+        :return: sock in operation
+                 bsh simulator in simulation
+        """
+        if self.sock is None:
+            s = self.createSock()
+            s.settimeout(2.0)
+            s.connect((self.host, self.port))
+
+            try:
+                self.sock = self.authenticate(sock=s)
+            except ValueError:
+                self.closeSock()
+                return self.connectSock()
+
+        return self.sock
 
     def createSock(self):
         if self.simulated:
@@ -122,6 +176,52 @@ class pdu(FSMDev, QThread, bufferedSocket.EthComm):
             s = bufferedSocket.EthComm.createSock(self)
 
         return s
+
+    def sendOneCommand(self, cmdStr, doClose=False, cmd=None, doRaise=True):
+        fullCmd = '%s%s' % (cmdStr, self.EOL)
+        reply = bufferedSocket.EthComm.sendOneCommand(self, cmdStr=cmdStr, doClose=doClose, cmd=cmd)
+
+        return self.parseResponse(cmd=cmd, fullCmd=fullCmd, reply=reply, doRaise=doRaise)
+
+    def parseResponse(self, cmd, fullCmd, reply, doRaise, retry=True):
+        if fullCmd in reply:
+            return reply.split(fullCmd)[1].strip()
+
+        if retry:
+            time.sleep(1)
+            reply = self.getOneResponse(cmd=cmd)
+            return self.parseResponse(cmd=cmd, fullCmd=fullCmd, reply=reply, doRaise=doRaise, retry=False)
+
+        if doRaise:
+            raise ValueError('Command was not echoed properly')
+
+        return
+
+    def authenticate(self, sock):
+        time.sleep(0.1)
+
+        ret = sock.recv(1024).decode('utf-8', 'ignore')
+
+        if 'Login: ' not in ret:
+            raise ValueError('Could not login')
+
+        sock.sendall('teladmin\r\n'.encode('utf-8'))
+
+        time.sleep(0.1)
+        ret = sock.recv(1024).decode('utf-8', 'ignore')
+
+        if 'Password:' not in ret:
+            raise ValueError('Bad login')
+
+        sock.sendall('toto\r\n'.encode('utf-8'))
+
+        time.sleep(0.1)
+        ret = sock.recv(1024).decode('utf-8', 'ignore')
+
+        if 'Logged in successfully' not in ret:
+            raise ValueError('Bad password')
+
+        return sock
 
     def handleTimeout(self):
         if self.exitASAP:
