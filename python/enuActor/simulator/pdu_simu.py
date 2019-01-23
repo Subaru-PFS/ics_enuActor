8e01d9f26176cb4bf72f8b102c5d0888c9b6a26c
Arnaud Le Fur <arnaud.lefur@lam.fr>
2019-01-23 12:26:12 +0100

    mimic dcb pdu behaviour

--- python/enuActor/simulator/pdu_simu.py
+++ python/enuActor/simulator/pdu_simu.py
@@ -1,13 +1,22 @@
-#!/usr/bin/env python
-
 import socket
 import time
 
 
 class PduSim(socket.socket):
+
     def __init__(self):
         socket.socket.__init__(self, socket.AF_INET, socket.SOCK_STREAM)
+        self.sendall = self.fakeSend
+        self.recv = self.fakeRecv
+
         self.buf = []
+        self.channels = {}
+        for nb in ['o%s' % (str(i + 1).zfill(2)) for i in range(15)]:
+            self.channels[nb] = 'off'
+
+        self.vals = {'volt': '110',
+                     'curr': '2',
+                     'pow': '220'}
 
     def connect(self, server):
         (ip, port) = server
@@ -17,16 +26,35 @@ class PduSim(socket.socket):
         if type(port) is not int:
             raise TypeError
 
-    def sendall(self, cmdStr, flags=None):
+        self.buf.append('Login: \r\n>')
+
+    def fakeSend(self, cmdStr):
+        time.sleep(0.1)
         cmdStr = cmdStr.decode()
 
-        self.buf.append('ok\r\n')
+        if cmdStr == 'teladmin\r\n':
+            self.buf.append('Password: \r\n>')
 
-    def recv(self, buffersize, flags=None):
+        elif cmdStr == 'toto\r\n':
+            self.buf.append('Logged in successfully \r\n>')
+
+        elif 'read status' in cmdStr:
+            __, __, nb, __ = cmdStr.split(' ')
+            self.buf.append('%s Outlet %s %s\r\n> ' % (cmdStr, nb[1:], self.channels[nb]))
+
+        elif 'read meter dev' in cmdStr:
+            __, __, __, val, __ = cmdStr.split(' ')
+            self.buf.append('%s %s\r\n\r\n> ' % (cmdStr, self.vals[val]))
+
+        elif 'sw o' in cmdStr:
+            __, nb, state, __ = cmdStr.split(' ')
+            self.channels[nb] = state
+            self.buf.append('%s  Outlet<%s> command is setting\r\n>' % (cmdStr, nb))
+
+    def fakeRecv(self, buffer_size):
         ret = self.buf[0]
         self.buf = self.buf[1:]
         return str(ret).encode()
-
 
     def close(self):
         pass
