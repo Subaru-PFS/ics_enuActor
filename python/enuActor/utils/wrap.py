75b0839d09147271b34d73c3ddf8e61359346b91
Arnaud Le Fur <arnaud.lefur@lam.fr>
2019-02-11 18:12:17 +0100

    prevent command to be queued except status

--- python/enuActor/utils/wrap.py
+++ python/enuActor/utils/wrap.py
@@ -4,19 +4,28 @@ from functools import partial
 
 def threaded(func):
     @putMsg
-    def wrapper(self, *args, **kwargs):
+    def wrapper(self, cmd, *args, **kwargs):
         try:
-            return func(self, *args, **kwargs)
+            self.actor.controllers[self.name].currCmd = cmd
+            return func(self, cmd, *args, **kwargs)
         except Exception as e:
-            cmd = args[0]
             cmd.fail('text=%s' % self.actor.strTraceback(e))
+        finally:
+            self.actor.controllers[self.name].currCmd = False
 
     return wrapper
 
+def isStatus(cmd):
+    return ' status' in cmd.rawCmd
+
 
 def putMsg(func):
-    def wrapper(self, *args, **kwargs):
-        self.actor.controllers[self.name].putMsg(partial(func, self, *args, **kwargs))
+    def wrapper(self, cmd, *args, **kwargs):
+        if self.actor.controllers[self.name].currCmd:
+            if not isStatus(cmd) and not isStatus(self.actor.controllers[self.name].currCmd):
+                raise RuntimeWarning('%s thread is busy' % self.name)
+
+        self.actor.controllers[self.name].putMsg(partial(func, self, cmd, *args, **kwargs))
 
     return wrapper
 
