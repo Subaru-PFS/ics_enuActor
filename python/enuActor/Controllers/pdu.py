e904551f6b5df93ed0949b32a55030441785c159
Arnaud Le Fur <afur@astro.princeton.edu>
2023-02-09 12:58:05 -0500

    read temperature and humidity sensors from pdu

--- python/enuActor/Controllers/pdu.py
+++ python/enuActor/Controllers/pdu.py
@@ -12,3 +12,22 @@ class pdu(aten.aten):
 
     def __init__(self, *args, **kwargs):
         aten.aten.__init__(self, *args, **kwargs)
+
+    def getStatus(self, cmd):
+        """Get envioall ports status.
+
+        :param cmd: current command.
+        :raise: Exception with warning message.
+        """
+        self.readEnvSensors(cmd)
+        aten.aten.getStatus(self, cmd)
+
+    def readEnvSensors(self, cmd):
+        """Get temps and humidity reading from the rack."""
+
+        try:
+            temps, humidity, NA = self.sendOneCommand('read sensor o01 simple', cmd=cmd).split('\r\n')
+        except:
+            temps, humidity = 'nan', 'nan'
+
+        cmd.inform(f'rackSensor={float(temps):.2f},{float(humidity):.2f}')
